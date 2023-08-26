3f37a552c828f23437494ce5ba51e5de88676dcc
AnastasiiaKalashnikova <nastasyakalashnikova@icloud.com>
2023-08-26 21:39:44 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients')
+
+const markup = ingredients.map((ingredient) => {
+  const li = document.createElement('li');
+  li.classList.add('item');
+  li.textContent = ingredient;
+  return li
+});
+
+list.append(...markup);
+console.log(list)
