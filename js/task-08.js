bbb4fa5084ec8ea0efdc03362f44d30325cfb32a
AnastasiiaKalashnikova <nastasyakalashnikova@icloud.com>
2023-08-27 13:10:28 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+    evt.preventDefault()
+    const { email, password } = evt.currentTarget.elements;
+    if (!(email.value.length)||!(password.value.length)) {
+        return alert('Please fill in all fields of the form')
+    } else {
+        const userData = {
+        Email : email.value,
+        Password: password.value
+        }
+        evt.currentTarget.reset()
+    return console.log(userData)
+    }
+}
