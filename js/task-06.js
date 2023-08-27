af0220c7ccefe04f01df21c95efbcd6b8227aa51
AnastasiiaKalashnikova <nastasyakalashnikova@icloud.com>
2023-08-27 19:27:42 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -4,7 +4,7 @@ const input = document.querySelector('#validation-input');
 input.addEventListener('blur', checkLength);
 
 function checkLength(evt) {
-    if (evt.currentTarget.value.length == evt.currentTarget.getAttribute('data-length')) {
+    if (evt.currentTarget.value.length === Number(evt.currentTarget.dataset.length)) {
         evt.currentTarget.classList.add('valid')
         evt.currentTarget.classList.remove('invalid')
     } else {
