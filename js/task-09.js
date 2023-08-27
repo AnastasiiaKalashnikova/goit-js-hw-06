33720c00bd6249f9d8545d173e1c775a66b4a6dc
AnastasiiaKalashnikova <nastasyakalashnikova@icloud.com>
2023-08-27 12:20:14 +0200

    9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btn = document.querySelector('.change-color')
+const color = document.querySelector('.color');
+const body = document.querySelector('body')
+
+btn.addEventListener('click', onClick);
+
+function onClick(evt) {
+  const currentColor = getRandomHexColor()
+  body.style.backgroundColor = currentColor;
+  color.textContent = currentColor;
+}
